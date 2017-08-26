36d825cdaec7168401a24771dbd04c4750c43c8c
mjlinville <31115829+mjlinville@users.noreply.github.com>
2017-08-26 14:40:20 -0600

    Update TextExamples.c

--- TextExamples.c
+++ TextExamples.c
@@ -2,5 +2,17 @@
 
 main()
 {
+  //print statements don't automatically get a new line so these two print functions will have the same output
+  hello()
+  altHello()
+}
+hello()
+{
   printf("Hello World\n");
+}
+altHello()
+{
+  printf("Hello ");
+  printf("World");
+  printf("\n");
 }
