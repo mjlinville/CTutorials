caba8e4802ca7c448afb2f9f2ccf25e03db10247
mjlinville <31115829+mjlinville@users.noreply.github.com>
2017-08-26 14:52:15 -0600

    Update TextExamples.c
    Added scanf and switch statements

--- TextExamples.c
+++ TextExamples.c
@@ -2,9 +2,21 @@
 
 main()
 {
-  //print statements don't automatically get a new line so these two print functions will have the same output
-  hello()
-  altHello()
+  char temp;
+  
+  scanf("%c",&temp);
+  
+  switch(temp) 
+  {
+    case 'h' :
+      //print statements don't automatically get a new line so these two print functions will have the same output
+      hello();
+      altHello();
+      break;
+    default :
+      printf("Invalid Input");
+  }
+
 }
 hello()
 {
