4d69abf1bed8e850cac36ca559696ae32d289fbd
mjlinville <31115829+mjlinville@users.noreply.github.com>
2017-08-31 19:29:39 -0600

    Added the Temperature Conversion example
    I expanded on the example in the text by doing the conversion to kelvin as well

--- TextExamples.c
+++ TextExamples.c
@@ -1,8 +1,17 @@
 #include stdio.h
 
-main()
+int change = 1;
+
+void Hello(void);
+void altHello(void);
+void convertTemp(int from, int to);
+int getParams(void);
+float tempChange(int f);
+
+void main(void)
 {
   char temp;
+  //int x, y;
   
   scanf("%c",&temp);
   
@@ -13,18 +22,53 @@ main()
       hello();
       altHello();
       break;
+    case 't' :
+      convertTemp(getParams(), getParams());
+      break;
     default :
       printf("Invalid Input");
   }
 
 }
-hello()
+void hello(void)
 {
   printf("Hello World\n");
 }
-altHello()
+void altHello(void)
 {
   printf("Hello ");
   printf("World");
   printf("\n");
 }
+void convertTemp(int from, int to)
+{
+  int i;
+  change = 0;
+  
+  printf("Temperature Conversion\n%10s %10s %10s \n","Fahrenheit","Celsius","Kelvin");
+  
+  for(i = from; i <= to; i += 5)
+  {
+    printf("%10d %10.2f %10.2f \n", i, tempChange);
+  }
+}
+float tempChange(int f)
+{
+  float i = (change)? (f - 32.0) * (5.0 / 9.0) : (f - 32.0) * (5.0 / 9.0) + 273;
+  return i;
+}
+int getParams(void)
+{
+  int temp;
+  
+  if(change) printf("From : ");
+  else printf("To : ");
+  
+  change = (change * -1) +1;
+  
+  return scanf("%d", temp);
+}
+             
+             
+             
+             
